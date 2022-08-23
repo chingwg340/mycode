6a86aa0e9f1a2e5fde0bf2435a348fac92d62472
chingwg340 <chingwg340@users.noreply.github.com>
2022-08-23 18:04:42 +0000

    my first commit

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Create a list containing three items
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# Display the first item in the lsit
+print("The first IP in the list: " + iplist[3])
+
+# Display the second item in the list
+print("The second IP in the list: " + iplist[4]) 
+
+
+
